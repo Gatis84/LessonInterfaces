cc9b683e057cf2d9d221e41a5d3e60f6eb3b890d
Gatis84 <gatis.paurans@gmail.com>
2022-07-12 23:22:25 +0300

    changed the way to do exercise

--- OlimpicGames.php
+++ OlimpicGames.php
@@ -1,34 +1,55 @@
 <?php
-interface Olimpics
+interface AthletesAction
 {
     public function throw():void;
 }
 
-class JavelinThrower implements Olimpics
+class JavelinThrower implements AthletesAction
 {
     public function throw(): void
     {
-        // TODO: Implement throw() method.
+        echo "Athlete throws javelin in " . rand(50,150) . "m distance" . PHP_EOL;
     }
 }
 
-class HammerThrower implements Olimpics
+class HammerThrower implements AthletesAction
 {
     public function throw(): void
     {
-        // TODO: Implement throw() method.
+        echo "Athlete throws hammer in " . rand(50,150) . "m distance" . PHP_EOL;
     }
 }
 
-class DiscThrower implements Olimpics
+class DiscThrower implements AthletesAction
 {
     public function throw(): void
     {
-        // TODO: Implement throw() method.
+        echo "Athlete throws disc in " . rand(50,150) . "m distance" . PHP_EOL;
     }
 }
 
-class Thrower
+class OlympicGames
 {
+    /**
+     * @param AthletesAction $athlete
+     * @return void
+     */
+    public function ReadySteadyGo($athlete):void
+    {
+        $athlete->throw();
+    }
+}
+
+$competition = new OlympicGames();
+
+$i=1;
+while ($i!=4)
+{
+    echo "*****ROUND $i *****" . PHP_EOL;
+    $competition->ReadySteadyGo($athlete1 = new HammerThrower());
+    $competition->ReadySteadyGo($athlete2 = new JavelinThrower());
+    $competition->ReadySteadyGo($athlete3 = new DiscThrower());
+    $i++;
+
 
 }
